8fd17e8b7c829f47970307fa001db27df69f370b
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-06-15 17:33:52 -0400

    Created User table

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 4000;
 server.use(express.json())
 
 
+
+
+
+server.get('/', (req,res)=> {
+    res.status(200).json({message: "server testing..."})
+})
 server.listen(port, () => {
     console.log(`\n*** Listening on Port ${port} *****\n`)
 })
